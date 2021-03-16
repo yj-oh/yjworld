cdc5a08ebea37eaa6202d7b97b8f39c333a0f49d
yj-oh <holidayoh99@gmail.com>
2021-03-17 02:42:12 +0900

    Init project (using yj-oh/react-boilerplate)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import { Route, Switch } from 'react-router-dom';
+
+//import Index from './index';
+
+const App = () => {
+	// return (
+	// 	<Switch>
+	// 		<Route component={Index} path='/' exact />
+	// 	</Switch>
+	// );
+};
+
+export default App;
